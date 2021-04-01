5c2631da5568715ecd28b439c6592d7ab7e2e23a
Ruslan_monar <ruslanmonar@gmail.com>
2021-04-02 02:06:43 +0300

    footer v1.1

---
+++ src/modules/Navigation/Footer.js
@@ -0,0 +1,71 @@
+import { React } from 'react';
+import classes from '../../css/style.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGoogle, faInstagram, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import {  } from '@fortawesome/react-fontawesome'
+import { faPhone , faSearchLocation , faEnvelope} from "@fortawesome/free-solid-svg-icons";
+
+export const Footer = () => {
+    return (
+        <div className={classes.footer}>
+            <div className={classes.contactInfo}>
+                <div className={classes.phoneNumber}>
+                    <FontAwesomeIcon icon={faPhone} size="lg" color="gray" />
+                    <div>
+                        <span className={classes.listItemTitle}>Цілодобова підтримка</span>
+                        <span className={classes.listItemTitle}>+380 689 4421</span>
+                    </div>
+                </div>
+                <div className={classes.phoneNumber}>
+                    <FontAwesomeIcon icon={faSearchLocation} size="lg" color="gray" />
+                    <div>
+                        <span className={classes.listItemTitle}>Адресса</span>
+                        <span className={classes.listItemTitle}>Львів</span>
+                    </div>
+                </div>
+                <div className={classes.phoneNumber}>
+                    <FontAwesomeIcon icon={faEnvelope} size="lg" color="gray" />
+                    <div>
+                        <span className={classes.listItemTitle}>Почта</span>
+                        <span className={classes.listItemTitle}>InternetShop@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+            <div className={classes.headerList}>
+                <div className={classes.listItem}>
+                    <span className={classes.listItemTitle}>BASE + SHOP</span>
+                    <span>Item 1</span>
+                    <span>Item 2</span>
+                    <span>Item 3</span>
+                </div>
+                <div className={classes.listItem}>
+                    <span className={classes.listItemTitle}>Rainshternles</span>
+                    <span>Item 1</span>
+                    <span>Item 2</span>
+                    <span>Item 3</span>
+                </div>
+                <div className={classes.listItem}>
+                    <span className={classes.listItemTitle}>Homaposder</span>
+                    <span>Item 1</span>
+                    <span>Item 2</span>
+                    <span>Item 3</span>
+                </div>
+                <div className={classes.listItem}>
+                    <span className={classes.listItemTitle}>SupeActronic</span>
+                    <span>Item 1</span>
+                    <span>Item 2</span>
+                    <span>Item 3</span>
+                </div>
+                <div className={classes.SocialMediaIconsFooter}>
+                    <span>Ми у соціальних мережах:</span>
+                    <div className={classes.iconItems}>
+                        <FontAwesomeIcon icon={faGoogle} size="lg" color="gray" />
+                        <FontAwesomeIcon icon={faInstagram} size="lg" color="gray" />
+                        <FontAwesomeIcon icon={faFacebook} size="lg" color="gray" />
+                        <FontAwesomeIcon icon={faYoutube} size="lg" color="gray" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
