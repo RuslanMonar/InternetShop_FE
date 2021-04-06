3c3c20be0ae7579e99fb04c7ca994b6d76c3a5d4
alexmaker20 <oleksadr0120@gmail.com>
2021-04-07 01:00:55 +0300

    simple search

--- src/modules/search_form/SearchForm.js
+++ src/modules/search_form/SearchForm.js
@@ -1,25 +1,23 @@
 import React, { useState } from "react";
 import axios from "axios";
 import classes from '../../css/style.module.css';
+import api from '../Auth/ApiAxios';
 
-const SearchForm = () => {
+const SearchForm =  () => {
     const [searchData, setSearchData] = useState("");
 
-    const searchFunc = () => {
-        if (searchData != "") {
 
-            fetch("https://jsonplaceholder.typicode.com/todos", {
-                method: 'POST',
-                body: JSON.stringify({
-                    searchItem: searchData
-                }),
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8",
-                },
+    const SearchFunc = () => {
+        let data = {"name":searchData}
+            api().post("/api/search", data).then(result => {
+                if (result.status === 200){
+                    console.log(result.data.search)
+                }
+            }).catch(err => {
+                console.log('Помилка');
             })
-            setSearchData("");
-        }
     }
+    
 
     function handle(event) {
         setSearchData(() => event.target.value);
@@ -27,7 +25,7 @@ const SearchForm = () => {
     return (
         <div className={classes.search}>
             <input onChange={handle} value={searchData} placeholder="Пошук"></input>
-            <div onClick = {() => searchFunc()}>
+            <div onClick = {() => SearchFunc()}>
                 <img src="/img/search-512.png" />
             </div>
         </div>
